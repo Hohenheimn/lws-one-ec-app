0118885d11a5f561e50791d0abb9e77a7c64b4da
kennethpole6 <polekenneth11@gmail.com>
2024-03-27 15:56:22 +0800

    feat(ui): header color and icon

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -9,17 +9,10 @@ const TabLayout = () => {
         options={{
           headerShown: false,
           tabBarShowLabel: false,
+          tabBarInactiveTintColor: "#707070",
+          tabBarActiveTintColor: "#86EFAC",
           tabBarIcon: ({ color, size }) => (
             <Octicons name="home" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="account"
-        options={{
-          tabBarShowLabel: false,
-          tabBarIcon: ({ color, size }) => (
-            <Octicons name="person" size={size} color={color} />
           ),
         }}
       />
@@ -27,8 +20,29 @@ const TabLayout = () => {
         name="transaction"
         options={{
           tabBarShowLabel: false,
+          headerTitle: "Transaction",
+          tabBarInactiveTintColor: "#707070",
+          tabBarActiveTintColor: "#86EFAC",
+          headerStyle: {
+            backgroundColor: "#86EFAC",
+          },
           tabBarIcon: ({ color, size }) => (
             <Octicons name="note" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          tabBarShowLabel: false,
+          headerTitle: "Profile",
+          tabBarInactiveTintColor: "#707070",
+          tabBarActiveTintColor: "#86EFAC",
+          headerStyle: {
+            backgroundColor: "#86EFAC",
+          },
+          tabBarIcon: ({ color, size }) => (
+            <Octicons name="person" size={size} color={color} />
           ),
         }}
       />
