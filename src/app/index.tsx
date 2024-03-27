f65842f899a6ae0ed89867576edd2ee7b97bc4cc
kennethpole6 <polekenneth11@gmail.com>
2024-03-27 10:37:10 +0800

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-3xl font-bold">HomeScreen</Text>
+      <Text className="text-3xl font-bold">TEST 1</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
