8620996a4b183a9288d88efa5a374b4710482bbb
kennethpole6 <polekenneth11@gmail.com>
2024-04-02 13:35:35 +0800

    feat: font, react query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Slot } from "expo-router";
-
-import { QueryClient, QueryClientProvider } from "react-query";
-
 import { SessionProvider } from "@/context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
