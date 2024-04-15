a8831ea12b74a79f51830fab25ca376ef87015f9
kennethpole6 <polekenneth11@gmail.com>
2024-04-15 17:24:31 +0800

    feat(ui): linking meter account

--- src/app/meter-details.tsx
+++ src/app/meter-details.tsx
@@ -1,16 +1,20 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Modal, Image } from "react-native";
+import React, { useState } from "react";
 import Description from "../components/Description";
 import Button from "../components/Button";
 import { useGetMeterAccount } from "../hooks/useGetMeterAccount";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import ScreenLoader from "../components/ScreenLoader";
 import ScreenError from "../components/ScreenError";
+import { useAddMeterAccount } from "../hooks/useAddMeterAccount";
 
 const MeterDetails = () => {
   const router = useRouter();
   const { coop, meterNumber } = useLocalSearchParams<any>();
   const { data, isLoading } = useGetMeterAccount(meterNumber, coop);
+  const { mutate, isPending, isError } = useAddMeterAccount();
+
+  const [isOpen, setIsOpen] = useState(false);
 
   if (isLoading) {
     return <ScreenLoader />;
@@ -19,6 +23,17 @@ const MeterDetails = () => {
   if (!data) {
     return <ScreenError message="No meter account found." />;
   }
+
+  if (isError) {
+    return <ScreenError message="Something went wrong." />;
+  }
+
+  const onSubmit = () => {
+    mutate(
+      { meterId: Number(data.data.meterNumber) },
+      { onSuccess: () => setIsOpen(true) }
+    );
+  };
 
   const meterAddress = data?.data.meterAddress.map((item) => {
     return {
@@ -34,23 +49,51 @@ const MeterDetails = () => {
   return (
     <View className="flex-1 space-y-4 bg-white p-4">
       <Text className="text-2xl font-poppins-sb mb-2">Meter Details</Text>
-
       <Description title="Meter Number" description={data?.data.meterNumber} />
       <Description
         title="Meter Account Name"
         description={data?.data.meterAccountName}
       />
       <Description title="Meter Address" description={meterAddress} />
-
       <Description title="Coop Name" description={data?.data.coop.coopName} />
       <Description title="Coop Address" description={coopAddress} />
-
-      <Button title="Link To My Account" appearance="primary" />
+      <Button
+        title="Link To My Account"
+        appearance="primary"
+        loading={isPending}
+        onPress={onSubmit}
+      />
       <Button
         title="Go Back"
         appearance="default"
         onPress={() => router.back()}
       />
+      <Modal visible={isOpen} onRequestClose={() => setIsOpen(false)}>
+        <View className="flex-1">
+          <View className="flex-1 items-center">
+            <Image
+              source={require("../../assets/images/message_success.png")}
+              className="w-72"
+              style={{ objectFit: "contain" }}
+            />
+          </View>
+          <View className="flex-1 p-4 space-y-4">
+            <Text className="text-2xl font-poppins-sb font-semibold text-center">
+              Account Link Successfully!
+            </Text>
+            <Text className="text-center text-lg">
+              Congratulations! With your account successfully linked, you now
+              have access to view your bills and monitor your power usage with
+              ease.
+            </Text>
+            <Button
+              title="Go back"
+              appearance="primary"
+              onPress={() => router.push("/home")}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
