47cb10ef77341cc5818308f9ed992bfa72e28289
Jomari Tiu <85975045+Hohenheimn@users.noreply.github.com>
2024-04-15 11:30:44 +0800

    fix:email verification, feat:profile modify, feat: change password, feat:bill history fronend

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/app/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/pageComponent/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
