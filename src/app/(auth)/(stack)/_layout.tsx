6ce57b08643e9069114d2b1e1041101e076608da
kennethpole6 <polekenneth11@gmail.com>
2024-04-17 13:38:04 +0800

    feat: status bar color

--- src/app/(auth)/(stack)/_layout.tsx
+++ src/app/(auth)/(stack)/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from "expo-router";
 
 const StackLayout = () => {
   return (
-    <Stack screenOptions={{ headerShadowVisible: false }}>
+    <Stack
+      screenOptions={{ headerShadowVisible: false, statusBarColor: "#86efac" }}
+    >
       <Stack.Screen
         name="account-details"
         options={{ headerTitle: "Account Details" }}
